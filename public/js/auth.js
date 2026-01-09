b908cbb676dc5c4e321eb660485479853a2956a2
Gabriel Silva <Gabriel.Silva6@br.disystalent.com>
2026-01-09 00:12:12 -0300

    Organizado2

---
+++ public/js/auth.js
@@ -0,0 +1,10 @@
+const token = localStorage.getItem("token");
+
+if (!token) {
+  window.location.href = "/login.html";
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "/login.html";
+}
