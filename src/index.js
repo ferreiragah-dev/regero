60aafc9fc7f9167c7ccbf191298d0ddfcd9f7583
Gabriel Silva <Gabriel.Silva6@br.disystalent.com>
2026-01-10 13:47:47 -0300

    Nova Pagina Cadastra acao

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import './cron/priceUpdater.js';
-
+import stocksRoutes from './routes/stocks.routes.js';
 import dashboardRoutes from './routes/dashboard.routes.js';
 import monitorRoutes from './routes/monitor.routes.js'; // 👈 IMPORTANTE
-
+app.use('/api', stocksRoutes);
 import marketRoutes from './routes/market.routes.js';
 
 const app = express();
